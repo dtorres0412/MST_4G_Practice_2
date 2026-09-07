b8ac365b73a3877ac4803186571b7c71603c4422
Daniel Torres <C2601015@pic.net.tw>
2026-09-07 11:05:49 +0800

    (feat)_Configured Program.cs with Swagger UI and InMemory DB

---
+++ Models/Zip.cs
@@ -0,0 +1,13 @@
+namespace MST_4G.Models;
+public class Zip
+{
+    public string ZipNo { get; set; } = string.Empty;
+    public string ZipName { get; set; } = string.Empty;
+    public DateTime EffDateFrom { get; set; }
+    public DateTime? EffDateTo { get; set; }
+
+    public string CountyNo { get; set; } = string.Empty;
+    public string ZoNo { get; set; } = string.Empty;
+    public string DoNo { get; set; } = string.Empty;
+
+}
