f143f8be6203dd98b50f6b6267940ed676bcd7cb
Daniel Torres <C2601015@pic.net.tw>
2026-09-07 15:16:50 +0800

    (feat)_Added Dto folder, ZipReadDto.cs and ZipSearchDto.cs

---
+++ Dto/ZipReadDto.cs
@@ -0,0 +1,18 @@
+namespace MST_4G.Dtos;
+
+public class ZipReadDto
+{
+    public string ZipNo { get; set; } = string.Empty;
+    public string ZipName { get; set; } = string.Empty;
+    public DateTime EffDateFrom { get; set; }
+    public DateTime? EffDateTo { get; set; }
+    
+    public string CountyNo { get; set; } = string.Empty;
+    public string CountyName { get; set; } = string.Empty;
+    
+    public string ZoNo { get; set; } = string.Empty;
+    public string ZoName { get; set; } = string.Empty;
+    
+    public string DoNo { get; set; } = string.Empty;
+    public string DoName { get; set; } = string.Empty;
+}
---
+++ Dto/ZipSearchDto.cs
@@ -0,0 +1,16 @@
+namespace MST_4G.Dtos;
+
+public class ZipSearchDto
+{
+    public string? ZipNo { get; set; }
+    public string? ZipName { get; set; }
+    public string? CountyNo { get; set; }
+
+     public string? CountyName { get; set; }
+    public string? ZoNo { get; set; }
+
+     public string? ZoName { get; set; }
+    public string? DoNo { get; set; }
+
+     public string? DoName { get; set; }
+}
