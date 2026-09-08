4e65f4e1f419bcda82a4609836241a718d7744df
Daniel Torres <C2601015@pic.net.tw>
2026-09-08 14:02:15 +0800

    (fix)_Added DoId to align with the database

--- Models/DistrictOffice.cs
+++ Models/DistrictOffice.cs
@@ -2,6 +2,7 @@ namespace MST_4G.Models;
 
 public class DistrictOffice
 {
+    public int DoId { get; set; }
     public string DoNo { get; set;} = string.Empty;
     public string DoName {get; set;} = string.Empty;
 }
