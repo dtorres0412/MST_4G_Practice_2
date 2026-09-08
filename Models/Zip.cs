54bf10a657db78ae1a21390e8570c4d4c4a15725
Daniel Torres <C2601015@pic.net.tw>
2026-09-08 14:03:06 +0800

    (fix)_Added ZipId, CountyId, ZoId, and DoId fields to align with the database

--- Models/Zip.cs
+++ Models/Zip.cs
@@ -1,14 +1,32 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MST_4G.Models;
+
 public class Zip
 {
-    public string ZipNo { get; set; } = string.Empty;
-    public string ZipName { get; set; } = string.Empty;
-    public DateTime EffDateFrom { get; set; }
-    public DateTime? EffDateTo { get; set; }
+    [Column("zip_id")]
+    public int ZipId { get; set; }
 
-    public string CountyNo { get; set; } = string.Empty;
-    public string ZoNo { get; set; } = string.Empty;
-    public string DoNo { get; set; } = string.Empty;
+    [Column("zip_no")]
+    public string ZipNo { get; set; } = string.Empty;
+
+    [Column("zip_name")]
+    public string ZipName { get; set; } = string.Empty;
+
+    [Column("eff_date_from")]
+    public DateTime EffDateFrom { get; set; }
+
+    [Column("eff_date_to")]
+    public DateTime EffDateTo { get; set; }
+
+    [Column("county_id")]
+    public int CountyId { get; set; }
+
+    [Column("zo_id")]
+    public int ZoId { get; set; }
+
+    [Column("do_id")]
+    public int DoId { get; set; }
 
     public County? County { get; set; }
     public Zo? Zo { get; set; }
