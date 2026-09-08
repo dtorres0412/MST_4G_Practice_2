d3f5a12170f6432108b3e99eeef695abbd28a4f5
Daniel Torres <C2601015@pic.net.tw>
2026-09-08 14:01:55 +0800

    (fix)_Added ZoId to align with the database

--- Models/Zo.cs
+++ Models/Zo.cs
@@ -2,6 +2,7 @@ namespace MST_4G.Models;
 
 public class Zo
 {
+    public int ZoId { get; set; }
     public string ZoNo { get; set;} = string.Empty;
     public string ZoName {get; set;} = string.Empty;
 }
