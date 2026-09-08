60d2e09ffe77fb8e489065f0b32628d4a79531f3
Daniel Torres <C2601015@pic.net.tw>
2026-09-08 16:14:39 +0800

    (feat)_Added UpdateZipDto for the Update API

---
+++ Dto/ZipUpdateDto.cs
@@ -0,0 +1,12 @@
+namespace MST_4G.Dtos;
+
+public class ZipUpdateDto
+{
+    public string ZipNo { get; set; } = string.Empty;
+    public string? ZipName { get; set; } = string.Empty;
+    public DateTime EffDateFrom { get; set; }
+    public DateTime EffDateTo { get; set; }
+    public int CountyId { get; set; }
+    public int ZoId { get; set; }
+    public int DoId { get; set; }
+}
