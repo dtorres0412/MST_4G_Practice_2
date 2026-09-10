6e7d33ac931865aa405bba1d7e0e9d0060462561
Daniel Torres <C2601015@pic.net.tw>
2026-09-10 15:30:43 +0800

    (fix)_Modified the AppDBContext.cs to include the Zip Junction table

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -7,62 +7,84 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<County> County { get; set; }
-    public DbSet<Zo> Zo { get; set; }
-    public DbSet<DistrictOffice> DistrictOffice { get; set; }
-    public DbSet<Zip> Zip { get; set; }
+    public DbSet<County> County { get; set; } = null!;
+    public DbSet<Zo> Zo { get; set; } = null!;
+    public DbSet<DistrictOffice> DistrictOffice { get; set; } = null!;
+    public DbSet<Zip> Zip { get; set; } = null!;
+    public DbSet<ZipJunction> ZipJunction { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Zip>().ToTable("zip");
-        modelBuilder.Entity<Zip>().HasKey(z => z.ZipId);
-        modelBuilder.Entity<Zip>().Property(z => z.ZipId).HasColumnName("zip_id");
-        modelBuilder.Entity<Zip>().Property(z => z.ZipNo).HasColumnName("zip_no");
-        modelBuilder.Entity<Zip>().Property(z => z.ZipName).HasColumnName("zip_name");
-        modelBuilder.Entity<Zip>().Property(z => z.EffDateFrom).HasColumnName("eff_date_from");
-        modelBuilder.Entity<Zip>().Property(z => z.EffDateTo).HasColumnName("eff_date_to");
-        modelBuilder.Entity<Zip>().Property(z => z.CountyId).HasColumnName("county_id");
-        modelBuilder.Entity<Zip>().Property(z => z.ZoId).HasColumnName("zo_id");
-        modelBuilder.Entity<Zip>().Property(z => z.DoId).HasColumnName("do_id");
+        modelBuilder.Entity<Zip>(entity =>
+        {
+            entity.ToTable("zip");
+            entity.HasKey(z => z.ZipId);
+            entity.Property(z => z.ZipId).HasColumnName("zip_id");
+            entity.Property(z => z.ZipNo).HasColumnName("zip_no");
+            entity.Property(z => z.ZipName).HasColumnName("zip_name");
+            entity.Property(z => z.EffDateFrom).HasColumnName("eff_date_from");
+            entity.Property(z => z.EffDateTo).HasColumnName("eff_date_to");
+        });
 
-        modelBuilder.Entity<County>().ToTable("county");
-        modelBuilder.Entity<County>().HasKey(c => c.CountyId);
-        modelBuilder.Entity<County>().Property(c => c.CountyId).HasColumnName("county_id");
-        modelBuilder.Entity<County>().Property(c => c.CountyNo).HasColumnName("county_no");
-        modelBuilder.Entity<County>().Property(c => c.CountyName).HasColumnName("county_name");
+        modelBuilder.Entity<County>(entity =>
+        {
+            entity.ToTable("county");
+            entity.HasKey(c => c.CountyId);
+            entity.Property(c => c.CountyId).HasColumnName("county_id");
+            entity.Property(c => c.CountyNo).HasColumnName("county_no");
+            entity.Property(c => c.CountyName).HasColumnName("county_name");
+        });
 
-        modelBuilder.Entity<Zo>().ToTable("zo");
-        modelBuilder.Entity<Zo>().HasKey(z => z.ZoId);
-        modelBuilder.Entity<Zo>().Property(z => z.ZoId).HasColumnName("zo_id");
-        modelBuilder.Entity<Zo>().Property(z => z.ZoNo).HasColumnName("zo_no");
-        modelBuilder.Entity<Zo>().Property(z => z.ZoName).HasColumnName("zo_name");
+        modelBuilder.Entity<Zo>(entity =>
+        {
+            entity.ToTable("zo");
+            entity.HasKey(z => z.ZoId);
+            entity.Property(z => z.ZoId).HasColumnName("zo_id");
+            entity.Property(z => z.ZoNo).HasColumnName("zo_no");
+            entity.Property(z => z.ZoName).HasColumnName("zo_name");
+        });
 
-        modelBuilder.Entity<DistrictOffice>().ToTable("district_office");
-        modelBuilder.Entity<DistrictOffice>().HasKey(d => d.DoId);
-        modelBuilder.Entity<DistrictOffice>().Property(d => d.DoId).HasColumnName("do_id");
-        modelBuilder.Entity<DistrictOffice>().Property(d => d.DoNo).HasColumnName("do_no");
-        modelBuilder.Entity<DistrictOffice>().Property(d => d.DoName).HasColumnName("do_name");
+        modelBuilder.Entity<DistrictOffice>(entity =>
+        {
+            entity.ToTable("district_office");
+            entity.HasKey(d => d.DoId);
+            entity.Property(d => d.DoId).HasColumnName("do_id");
+            entity.Property(d => d.DoNo).HasColumnName("do_no");
+            entity.Property(d => d.DoName).HasColumnName("do_name");
+        });
 
-        modelBuilder.Entity<County>().HasKey(c => c.CountyId);
-        modelBuilder.Entity<Zo>().HasKey(z => z.ZoId);
-        modelBuilder.Entity<DistrictOffice>().HasKey(d => d.DoId);
-        modelBuilder.Entity<Zip>().HasKey(z => z.ZipId);
+        modelBuilder.Entity<ZipJunction>(entity =>
+        {
+            entity.ToTable("zip_junction");
 
-        modelBuilder.Entity<Zip>()
-            .HasOne(z => z.County)
-            .WithMany()
-            .HasForeignKey(z => z.CountyId);
+            entity.HasKey(zj => new { zj.ZipNo, zj.CountyNo, zj.ZoNo, zj.DoNo });
 
-        modelBuilder.Entity<Zip>()
-            .HasOne(z => z.Zo)
-            .WithMany()
-            .HasForeignKey(z => z.ZoId);
+            entity.Property(zj => zj.ZipNo).HasColumnName("zip_no");
+            entity.Property(zj => zj.CountyNo).HasColumnName("county_no");
+            entity.Property(zj => zj.ZoNo).HasColumnName("zo_no");
+            entity.Property(zj => zj.DoNo).HasColumnName("do_no");
 
-        modelBuilder.Entity<Zip>()
-            .HasOne(z => z.DistrictOffice)
-            .WithMany()
-            .HasForeignKey(z => z.DoId);
+            entity.HasOne(zj => zj.Zip)
+                  .WithMany(z => z.ZipJunction)
+                  .HasForeignKey(zj => zj.ZipNo)
+                  .HasPrincipalKey(z => z.ZipNo);
+
+            entity.HasOne(zj => zj.County)
+                  .WithMany()
+                  .HasForeignKey(zj => zj.CountyNo)
+                  .HasPrincipalKey(c => c.CountyNo);
+
+            entity.HasOne(zj => zj.Zo)
+                  .WithMany()
+                  .HasForeignKey(zj => zj.ZoNo)
+                  .HasPrincipalKey(z => z.ZoNo);
+
+            entity.HasOne(zj => zj.DistrictOffice)
+                  .WithMany()
+                  .HasForeignKey(zj => zj.DoNo)
+                  .HasPrincipalKey(d => d.DoNo);
+        });
     }
 }
