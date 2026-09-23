f59acce7475b29ae4368d8ada4a74c302bbb9390
Daniel Torres <C2601015@pic.net.tw>
2026-09-23 16:28:28 +0800

    (feat)_Added the two enpoints for querying and adding endpoints for County in the newly created CountyController.cs file

---
+++ Controllers/CountyController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using MST_4G.Services;
+using MST_4G.Dtos;
+
+namespace MST_4G.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountyController(ICountyService countyService) : ControllerBase
+{
+     [HttpGet("{countyNo}")]
+    public async Task<ActionResult<CountyReadDto>> GetByCountyNo(string countyNo)
+    {
+        var County = await countyService.GetByCountyNoAsync(countyNo);
+
+        if (County == null)
+            return NotFound($"County record with CountyNo '{countyNo}' not found.");
+
+        return Ok(County);
+    }
+
+    [HttpPost("create")]
+    public async Task<ActionResult<CountyReadDto>> CreateCounty([FromBody] CountyCreateDto createCountyDto)
+    {
+        try
+        {
+            var result = await countyService.CreateCountyAsync(createCountyDto);
+
+            return CreatedAtAction(nameof(GetByCountyNo), new {countyNo = result?.CountyNo}, result);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+    }
+    
+}
