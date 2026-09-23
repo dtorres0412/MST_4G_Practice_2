ae2da839ea537dbd0c9c65b47d4122be4b390cd5
Daniel Torres <C2601015@pic.net.tw>
2026-09-23 16:30:18 +0800

    (feat)_Added CountyDto folder with its CountyCreateDto and CountyReadDto files

---
+++ Dto/CountyDto/CountyCreateDto.cs
@@ -0,0 +1,8 @@
+namespace MST_4G.Dtos;
+
+public class CountyCreateDto
+{
+    public string CountyNo { get; set; } = string.Empty;
+    public string? CountyName { get; set; } = string.Empty;
+
+}
---
+++ Dto/CountyDto/CountyReadDto.cs
@@ -0,0 +1,8 @@
+namespace MST_4G.Dtos;
+
+public class CountyReadDto
+{
+    public int CountyId { get; set; }
+    public string CountyNo { get; set; } = string.Empty;
+    public string CountyName { get; set; } = string.Empty;
+}
