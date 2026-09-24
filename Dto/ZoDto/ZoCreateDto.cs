a7030cd4f25ac81dbe4ea6b8370a4984a036cc3d
Daniel Torres <C2601015@pic.net.tw>
2026-09-24 14:43:59 +0800

    (feat)_Added the ZoDto folder together with ZoCreateDto.cs and ZoReadDto.cs files

---
+++ Dto/ZoDto/ZoCreateDto.cs
@@ -0,0 +1,8 @@
+namespace MST_4G.Dtos;
+
+public class ZoCreateDto
+{
+    public string ZoNo { get; set; } = string.Empty;
+    public string? ZoName { get; set; } = string.Empty;
+
+}
---
+++ Dto/ZoDto/ZoReadDto.cs
@@ -0,0 +1,9 @@
+
+namespace MST_4G.Dtos;
+
+public class ZoReadDto
+{
+    public int ZoId { get; set; }
+    public string ZoNo { get; set; } = string.Empty;
+    public string ZoName { get; set; } = string.Empty;
+}
